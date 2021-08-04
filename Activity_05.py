03de4f94cded15ed505f7cc40cb8dd938f5664fb
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-04 21:05:19 +0530

    Rechecked_commit

--- Activity_05.py
+++ Activity_05.py
@@ -1,21 +1,7 @@
-a = int(input("Enter value:"))
-b = int(input("Enter value:"))
-c = int(input("Enter value:"))
-d = int(input("Enter value:"))
-e = int(input("Enter value:"))
-list_sum = [a,b,c,d,e]
+lst = input("Enter the numbers:")
+num = lst.split()
+print(num)
 sum = 0
-for i in list_sum:
-    sum =sum +int(i)
-print(sum)    
-//2-Found an another way to take user input and sum    
-lst=[]
-n = int(input("Enter the number of elements in the list\n"))
-for i in range(0,n):
-    ele = int(input("Enter the number\n"))
-    lst.append(ele)
-print(lst)    
-sum = 0
-for i in lst:
-    sum = sum +int(i)
+for i in num:
+    sum = sum + int(i)
 print(sum)    
