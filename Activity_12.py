72ee33cb522eb2b9d8bf81b7a1478c46bb3692a5
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-08 13:08:55 +0530

    D0necommit

---
+++ Activity_12.py
@@ -0,0 +1,17 @@
+def input_number():
+    num = int(input("Enter the number"))
+    return num
+def compare(a,b,c):
+    if (a<b)and(c<b):
+        print(f"{b} is the greatest number")
+    elif (c>b)and(c>a):
+        print(f"{c} is the greatest number")
+    else:
+        print(f"{a} is the greatest number")
+def main():
+    a = input_number()
+    b = input_number()
+    c = input_number()
+    compare(a,b,c)
+
+main()
