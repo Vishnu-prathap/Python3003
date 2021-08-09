5c11ff161bf4ad2daaceed4d2129e60e2233a6a2
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-09 21:07:25 +0530

    Commit

--- Activity_04.py
+++ Activity_04.py
@@ -1,6 +1,6 @@
 a = int(input("Enter first number:\n"))
 b = int(input("Enter second number:\n"))
 c = a+b
-print(f'The sum is {c}')
+print(f'The sum of {a}+{b} = {c}')
 print(c)
 print("The sum of %d and %d is %d"%(a,b,c))
