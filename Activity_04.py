c38294279fe88f803737fec792fca4ed440521d5
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-10 10:59:02 +0530

    print3

--- Activity_04.py
+++ Activity_04.py
@@ -2,5 +2,7 @@ a = int(input("Enter first number:\n"))
 b = int(input("Enter second number:\n"))
 c = a+b
 print(f'The sum of {a}+{b} = {c}')
-print("The sum ",a,"+",b,"=",c)
+print("The sum ",a,"plus",b,"=",c)
 print("The sum of %d and %d is %d"%(a,b,c))
+txt = "The sum of {}+{}={}".format(a,b,c)
+print(txt)
