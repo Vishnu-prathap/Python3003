65503349b839a909c6c748e48abb8f4292378db4
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-14 21:55:11 +0530

    updatecommit

--- Activity_13.py
+++ Activity_13.py
@@ -1,3 +1,4 @@
+
 import math
 def input_num():
     num = int(input("Enter a number "))
@@ -9,16 +10,21 @@ def checkforprime(num):
         for i in range(2,int(math.sqrt(num))):
             if (num%i)== 0:
                 flag = 0
+    return flag
+def output(flag,num):
+             
         if(flag==1):
             print(f"{num} is a prime number\n")
             
                     
         else:
             print(f"{num} is not a prime number\n")
-             
-             
+    
+    
+    
 def main():
     a = input_num()
     c = checkforprime(a)
+    d = output(c,a)
 main()
 
