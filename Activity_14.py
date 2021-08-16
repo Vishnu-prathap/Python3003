bf419c8742b0ffbf81b34ecbc5e983e60d4125fa
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-08-16 19:03:33 +0530

    final_commit

--- Activity_14.py
+++ Activity_14.py
@@ -1,26 +1,33 @@
 def input_dimensions():
-    num = int(input("Enter the values of length, breadth and Height of tromboloid: "))
+    num = input("Enter the values of length, breadth and Height of tromboloid: ")
+    x = num.split()
+    m =[]
+    for i in x:
+        ele = float(i)
+        m.append(ele)
+    return m
     
-    return int(num.split())
-def valk(l,b,h):
-    k = l**2+b**2+h**2
+def valk(m):
+    k = m[0]**2+m[1]**2+m[2]**2
     return k
-def vol(l,b,h,k):
-    v = ((b**2)*(h**2))/(k**1/2)
+
+def vol(m,k):
+    v = ((m[1]**2)*(m[2]**2))/(k**1/2)
     return v
+
 def radius(v):
     r = ((v*(3/4))/3.14)**(1/3)
     return r
+
 def main():
+    
     l = input_dimensions()
-    b = input_dimensions()
-    h = input_dimensions()
-    k = valk(l,b,h)
-    volume = vol(l,b,h,k)
+    k = valk(l)
+    volume = vol(l,k)
     print("The volume of the trombolod is: ",volume)
     r = radius(volume)
     print("Radius of sphere with same volume as tromboloid: ",r)
     
-    
 main()
+
 
