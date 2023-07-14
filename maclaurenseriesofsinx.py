19b7b6e85e4932c7416f5e0191ffd8e26d0dccd6
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2023-07-14 22:06:02 +0530

    updated commit

--- maclaurenseriesofsinx.py
+++ maclaurenseriesofsinx.py
@@ -1,9 +1,5 @@
-n = 10 
-sum = 0
-sign = 1 
-
 def input_theta():
-  theta = float(input("Enter value of X\n"))
+  theta = float(input("Enter the angle for whose sin is to be calculated (in radians)\n"))
   return theta
 
 def factorial(n):
@@ -12,17 +8,19 @@ def factorial(n):
     fac=fac*i
   return fac
 
-def sinxvalue(n,sign,theta,sum):
+def sinxvalue(n,theta,sum):
+  sum = 0
+  sign = 1 
   for i in range(1,n):
     sum = sum + sign*(theta**(2*i-1)/factorial(2*i-1))
     sign = (-1)*sign
   return sum
 
 def main():
+  n = 10 
   a = input_theta()
-  b = sinxvalue(n,sign,a,sum)
-  print(b)
+  b = sinxvalue(n,a,sum)
+  print("The value of sinx = ",b)
   return 
 
 main()
-
