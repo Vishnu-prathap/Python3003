df49d00bccb07df4c793f83d44c80504d26169ef
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2023-07-17 23:26:47 +0530

    Update tupletodict commit

--- tupletodict.py
+++ tupletodict.py
@@ -1,6 +1,12 @@
 def convert_to_dict(tuple_list):
-	dictionary = dict((key, value) for key, value in tuple_list)
+	dictionary = {}
+	for tuple in tuple_list:
+		if tuple[0] in dictionary:
+			dictionary[tuple[0]].append(tuple[1])
+		else:
+			dictionary[tuple[0]] = [tuple[1]]
 	return dictionary
+
 
 
 tuple_list = [("akash", 10), ("gaurav", 12), ("anand", 14),
