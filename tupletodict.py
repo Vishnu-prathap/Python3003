57fa7bee65206aa6c325a8cb034b163bd4ad320e
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2023-07-17 13:40:47 +0530

    Updated commit

--- tupletodict.py
+++ tupletodict.py
@@ -1,9 +1,9 @@
-tups = [("akash", 10), ("gaurav", 12), ("anand", 14),
-		("suraj", 20), ("akhil", 25), ("ashish", 30)]
-def tupletodict(tups):
-  dictionary = {}
-  for key, val in tups:
-    dictionary.setdefault(key, val)
-  return dictionary
+def convert_to_dict(tuple_list):
+	dictionary = dict((key, value) for key, value in tuple_list)
+	return dictionary
 
-print("The tuple is converted into the following dictionary",tupletodict(tups))
+
+tuple_list = [("akash", 10), ("gaurav", 12), ("anand", 14),
+			("suraj", 20), ("akhil", 25), ("ashish", 30)]
+
+print(convert_to_dict(tuple_list))
